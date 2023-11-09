2fcd47abe807ed33ded16ef8c25b234657eb668b
Asier Villar <asiervillar@outlook.es>
2023-11-09 17:48:51 +0100

    ADD: now images have white background

--- src/main.py
+++ src/main.py
@@ -5,39 +5,45 @@ from imutils.perspective import four_point_transform  # type: ignore
 
 
 def main():
-    img = cv2.imread("imagenes/foto4.jpg")
+    image_path = Path() / "imagenes" / "foto5.jpg"
+    img = cv2.imread(image_path.as_posix())
 
     # Obtenemos el contorno.
-    document_contour = scanDetection(img)
+    pape_contour, document_contour = scanDetection(img)
+    processed = image_processing(img, pape_contour, document_contour)
 
-    wraped = four_point_transform(img.copy(), document_contour.reshape(4, 2))
-    processed = image_processing(wraped)
+    cv2.imwrite("prueba1.jpg", processed)
+    render_images(img, processed)
 
-    print(processed.shape)
-    print(processed)
 
-    cv2.imshow("Processed", cv2.pyrDown(cv2.pyrDown(processed)))
+def render_images(img, processed):
+    # Se muestra la imagen procesada
+    cv2.imshow("Processed", img_downscale(processed, 2))
     cv2.moveWindow("Processed", 1000, 1)
 
-    # cv2.imshow("Wraped", cv2.pyrDown(cv2.pyrDown(wraped)))
-    # cv2.moveWindow("Wraped", 1000, 0)
-
-    img = cv2.pyrDown(img)
-    # img = cv2.pyrDown(img)
-    img = cv2.pyrDown(img)
-    cv2.imshow("foto1", img)
+    # Se muestra la imagen original
+    cv2.imshow("foto1", img_downscale(img, 3))
     cv2.moveWindow("foto1", 0, 1)
     cv2.waitKey(0)
 
-    # cv2.imwrite("prueba1.jpg", processed)
+
+def get_img_contour(img):
+    height, width, _ = img.shape
+    document_contour = np.array([[0, 0], [width, 0], [width, height], [0, height]])
+
+    return document_contour
+
+
+def img_downscale(img, scale=1):
+    new_img = img.copy()
+    for _ in range(scale):
+        new_img = cv2.pyrDown(new_img)
+
+    return new_img
 
 
 def scanDetection(img):
-    # Modificar esto según la resolución de la cámara.
-    HEIGHT, WIDTH, _ = img.shape
-
-    # Si no hay contorno, que pille toda la imagen.
-    document_contour = np.array([[0, 0], [WIDTH, 0], [WIDTH, HEIGHT], [0, HEIGHT]])
+    document_contour = get_img_contour(img)
 
     # Aplicamos los filtros.
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -58,16 +64,22 @@ def scanDetection(img):
 
     # Se dibuja el contorno.
     rect = cv2.minAreaRect(document_contour)
-    box = np.int0(cv2.boxPoints(rect))
+    box = np.intp(cv2.boxPoints(rect))
+    # cv2.drawContours(img, [document_contour], 0, (255, 255, 255), 100)
 
-    cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-    cv2.drawContours(img, [document_contour], -1, (255, 0, 0), 3)
-
-    return box
+    return box, document_contour
 
 
-def image_processing(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def image_processing(image, page_contour, document_contour):
+    aux_img = image.copy()
+    cv2.fillPoly(aux_img, [page_contour], (255, 255, 255))
+    cv2.fillPoly(aux_img, [document_contour], (0, 0, 0))
+    # cv2.drawContours(aux_img, [document_contour], 0, (255, 255, 255), 150)
+
+    result = cv2.add(aux_img, image)
+    wrapped = four_point_transform(result.copy(), page_contour.reshape(4, 2))
+
+    gray = cv2.cvtColor(wrapped, cv2.COLOR_BGR2GRAY)
     _, threshold = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
 
     return threshold
